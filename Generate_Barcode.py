aff4b38d1da366a1fc6ab5a889528c67203c5388
navanee1407 <navanee1407@gmail.com>
2026-07-29 12:15:57 +0530

    updated main

--- Generate_Barcode.py
+++ Generate_Barcode.py
@@ -3,13 +3,11 @@ import barcode
 from barcode.writer import ImageWriter
 import mysql.connector
 
-mydb = mysql.connector.connect(host='localhost',user='root',password='admin')
+mydb = mysql.connector.connect(host='localhost',user='root',password='admin',database='school')
 mycursor = mydb.cursor()
 
-mycursor.execute('Create database School')
-mycursor.execute('Create table Student_details(
-rollNumber int
-studentName varchar)')
+create_table_query='create table if not exists Student_details(rollNumber int(5) primary key, studentName varchar(50))'
+mycursor.execute(create_table_query)
 
 rollNumber = input("Enter the student roll number: ")
 studentName = input('Enter the student name:')
@@ -24,3 +22,8 @@ filename = os.path.join(script_folder, f"student_{rollNumber}")
 saved_path = my_barcode.save(filename)
 
 print(f"Barcode successfully saved as: {saved_path}")
+
+insert_query ='insert into Student_Details values (%s,%s)'
+insert_values= (rollNumber,studentName)
+mycursor.execute(insert_query,insert_values)
+mydb.commit()
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import mysql.connector
 import datetime
 
-mydb = mysql.connector.connect(host='localhost',user='root',password='admin',database='School')
+mydb=mysql.connector.connect(host='localhost',user='root',password='admin',database='student_details')
+mycursor=mydb.cursor()
+
+today=datetime.datetime.now()
+
+rollNumber=input('Scan the barcode:')
+
