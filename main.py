4ffc87dd94556a533e810fc5c96ed0a1746aa564
navanee1407 <navanee1407@gmail.com>
2026-08-03 19:28:07 +0530

    main.py updated(1)

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import mysql.connector
 import datetime
+from datetime import date
 
-mydb=mysql.connector.connect(host='localhost',user='root',password='admin',database='student_details')
+
+mydb=mysql.connector.connect(host='localhost',user='root',password='admin',database='school')
 mycursor=mydb.cursor()
 
-today=datetime.datetime.now()
+today=datetime.datetime.now().strftime('%d/%m/%Y-%H:%M')
+today_table_name = datetime.date.today().strftime('%d/%m/%Y')
+
+create_today_table_query = f""" 
+create table if not exists `{today_table_name}`(
+    rollnumber int(5),
+    studentName varchar(50),
+    time varchar(10),
+    Status varchar(10)
+)
+"""
+mycursor.execute(create_today_table_query)
 
 rollNumber=input('Scan the barcode:')
+
+select_query='select * from student_details'
+mycursor.execute(select_query)
+data=mycursor.fetchall()
+for i in data:
+    print(i)
+
 
